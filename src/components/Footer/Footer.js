4f67b274a6dbd99fc5fd085474cbd9740766553c
marikamulder <113202631+marikamulder@users.noreply.github.com>
2023-02-20 12:08:17 -0500

    header, footer, and styling.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div><a href="https://github.com/marikamulder/quiz-book">Coded</a> by Marika Mulder</div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="header">
+            <div>Quiz Taking App</div>
+        </header>
+    );
+}
+
+export default Header;
